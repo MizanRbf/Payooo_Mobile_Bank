0f12ccfda69f2e9fbe5ba2e20e2ba1ccff265a0c
MizanRbf <mizanrbf@gmail.com>
2025-02-26 09:53:04 +0600

    updated

--- index2.js
+++ index2.js
@@ -1,13 +1,3 @@
-document.getElementById('add-btn').addEventListener('click', function(event){
-event.preventDefault();
-const amountAdd = document.getElementById('amount').value;
-const convertedAmount = parseInt(amountAdd);
-const amountMoney = document.getElementById('amount-money').innerText;
-const convertedMoney = parseInt(amountMoney);
-const sum = convertedMoney + convertedAmount;
-document.getElementById('amount-money').innerText = sum;
-})
-
 // Add Money
 
 document.getElementById('card-1').addEventListener('click', function(event){
@@ -18,6 +8,8 @@ const transferSection = document.getElementById('transfer-section');
 const getBonus = document.getElementById('bonus-section');
 const payBill = document.getElementById('pay-section');
 const historySection = document.getElementById('history-section');
+const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'none';
   payBill.style.display = 'none';
   getBonus.style.display = 'none';
@@ -36,6 +28,8 @@ document.getElementById('card-2').addEventListener('click', function(event){
   const getBonus = document.getElementById('bonus-section');
   const payBill = document.getElementById('pay-section');
   const historySection = document.getElementById('history-section');
+  const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'none';
   payBill.style.display = 'none';
   getBonus.style.display = 'none';
@@ -54,6 +48,8 @@ document.getElementById('card-3').addEventListener('click', function(event){
   const getBonus = document.getElementById('bonus-section');
   const payBill = document.getElementById('pay-section');
   const historySection = document.getElementById('history-section');
+  const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'none';
   payBill.style.display = 'none';
   getBonus.style.display = 'none';
@@ -72,6 +68,8 @@ document.getElementById('card-4').addEventListener('click', function(event){
   const getBonus = document.getElementById('bonus-section');
   const payBill = document.getElementById('pay-section');
   const historySection = document.getElementById('history-section');
+  const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'none';
   payBill.style.display = 'none';
   getBonus.style.display = 'block';
@@ -91,6 +89,8 @@ document.getElementById('card-5').addEventListener('click', function(event){
   const getBonus = document.getElementById('bonus-section');
   const payBill = document.getElementById('pay-section');
   const historySection = document.getElementById('history-section');
+  const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'none';
   payBill.style.display = 'block';
   getBonus.style.display = 'none';
@@ -110,6 +110,8 @@ document.getElementById('card-6').addEventListener('click', function(event){
   const getBonus = document.getElementById('bonus-section');
   const payBill = document.getElementById('pay-section');
   const historySection = document.getElementById('history-section');
+  const latestPayment = document.getElementById('latest');
+latestPayment.style.display = 'none';
   historySection.style.display = 'block';
   payBill.style.display = 'none';
   getBonus.style.display = 'none';
@@ -122,4 +124,79 @@ document.getElementById('card-6').addEventListener('click', function(event){
 document.getElementById('logout').addEventListener('click', function(event){
   event.preventDefault();
   window.location.href = './index.html';
+})
+
+// Add Money
+
+document.getElementById('add-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  const addInputThree = document.getElementById('add-input-3').value;
+  const convertedInputThree = parseInt(addInputThree);
+  const totalMoney = document.getElementById('total-money').innerText;
+  const convertedTotalMoney = parseInt(totalMoney);
+  const addInputTwo = document.getElementById('add-input-2').value;
+  const addInputFour = document.getElementById('add-input-4').value;
+  const sum = convertedInputThree + convertedTotalMoney;
+
+  if(addInputTwo.length === 11 && addInputFour === '1234'){
+    document.getElementById('total-money').innerText = sum;
+  }
+  else{
+    alert('num or password invalid')
+  }
+})
+
+// Cash Out
+document.getElementById('cash-btn').addEventListener('click', function(event){
+  event.preventDefault();
+const totalMoney = document.getElementById('total-money').innerText;
+const cashInputOne = document.getElementById('cash-input-1').value;
+const cashInputTwo = document.getElementById('cash-input-2').value;
+const cashInputThree = document.getElementById('cash-input-3').value;
+const restMoney = totalMoney - cashInputTwo;
+if(cashInputOne.length === 11 && cashInputThree === '1234'){
+  document.getElementById('total-money').innerText = restMoney;
+}
+else{
+  alert('num or pin invalid')
+}
+})
+
+// Transfer Money
+document.getElementById('transfer-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  const totalMoney = document.getElementById('total-money').innerText;
+  const transferInputOne = document.getElementById('transfer-input-1').value;
+  const transferInputTwo = document.getElementById('transfer-input-2').value;
+  const transferInputThree = document.getElementById('transfer-input-3').value;
+  const restMoney2 = totalMoney - transferInputTwo;
+  if(transferInputOne.length === 11 && transferInputThree === '1234'){
+    document.getElementById('total-money').innerText = restMoney2;
+  }
+  else{
+    alert('num or pin invalid')
+  }
+  
+})
+
+// Get Bonus
+document.getElementById('bonus-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  alert('bonus');
+  
+})
+
+// Pay Bill
+document.getElementById('pay-btn').addEventListener('click', function(event){
+  event.preventDefault();
+  alert('bonus');
+  
+})
+
+// Transaction
+
+document.getElementById('transaction-btn').addEventListener('click', function(event){
+  event.preventDefault();
+
+  
 })
